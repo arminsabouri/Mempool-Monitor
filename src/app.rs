88baf9e160cb8431ab9a92e1f0c2c553dc33f69b
Armin Sabouri <armins88@gmail.com>
2025-03-03 22:19:29 -0500

    Store rbf updates

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,8 @@
 use crate::database::Database;
 use anyhow::Result;
-use bitcoin::{consensus::Decodable, Transaction};
+use bitcoin::{consensus::Decodable, Amount, Transaction};
 use bitcoincore_zmq::MessageStream;
-use bitcoind::bitcoincore_rpc::Client;
+use bitcoind::bitcoincore_rpc::{Client, RpcApi};
 use futures_util::StreamExt;
 use log::info;
 
@@ -17,6 +17,20 @@ impl App {
         Self { bitcoind, zmq, db }
     }
 
+    fn get_transaction_fee(&self, tx: &Transaction) -> Result<Amount> {
+        // TODO: this should be handled on a non blocking thread. This could halt if there are many inputs
+        let out_total = tx.output.iter().map(|o| o.value).sum::<Amount>();
+        let mut in_total = Amount::ZERO;
+        for input in tx.input.iter() {
+            // TODO: handle coinbase inputs
+            let ot = input.previous_output;
+            let prev_tx = self.bitcoind.get_transaction(&ot.txid, None)?;
+            let prev_out = prev_tx.transaction()?.output[ot.vout as usize].value;
+            in_total += prev_out;
+        }
+        Ok(in_total - out_total)
+    }
+
     pub async fn run(&mut self) -> Result<()> {
         info!("Starting mempool tracker");
         while let Some(message) = self.zmq.next().await {
@@ -28,13 +42,21 @@ impl App {
                     let tx_bytes = message.serialize_data_to_vec();
                     let tx = Transaction::consensus_decode(&mut tx_bytes.as_slice())?;
                     let txid = tx.compute_txid();
-                    if self.db.tx_exists(&txid)? {
+                    if let Some(txid) = self.db.inputs_hash(tx.clone().input)? {
+                        info!("Transaction was RBF'd: {:?}", txid);
+                        let fee = self.get_transaction_fee(&tx)?;
+                        info!("Fee: {}", fee);
+                        self.db.record_rbf(tx, fee.to_sat())?;
+                        continue;
+                    }
+
+                    if self.db.txid_exists(&txid)? {
                         self.db.record_mined_tx(&txid)?;
                         info!("Transaction already exists: {:?}", txid);
-                    } else {
-                        self.db.insert_mempool_tx(tx)?;
-                        info!("Transaction inserted: {:?}", txid);
+                        continue;
                     }
+                    self.db.insert_mempool_tx(tx)?;
+                    info!("Transaction inserted: {:?}", txid);
                 }
                 Err(e) => {
                     return Err(e.into());
--- src/database.rs
+++ src/database.rs
@@ -1,18 +1,27 @@
 use std::time::SystemTime;
 
 use anyhow::Result;
-use bitcoin::{hashes::Hash, Transaction, Txid};
+use bitcoin::{consensus::Encodable, hashes::Hash, Transaction, TxIn, Txid};
+use bitcoin_hashes::Sha256;
 use serde::{Deserialize, Serialize};
 
 pub struct Database(sled::Db);
 
 const TX_INDEX_KEY: &[u8; 6] = b"tx_idx";
+const INPUTS_HASH_KEY: &[u8; 7] = b"in_hash";
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct RBFInner {
+    created_at: SystemTime,
+    fee_total: u64,
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct TransactionInner {
     pub inner: Transaction,
     pub found_at: SystemTime,
     pub mined_at: SystemTime,
+    rbf_inner: Vec<RBFInner>,
 }
 
 impl TransactionInner {
@@ -21,6 +30,7 @@ impl TransactionInner {
             inner: tx,
             found_at: SystemTime::now(),
             mined_at: SystemTime::UNIX_EPOCH,
+            rbf_inner: vec![],
         }
     }
 }
@@ -31,7 +41,13 @@ impl Database {
         Ok(Self(db))
     }
 
-    pub(crate) fn tx_exists(&self, txid: &Txid) -> Result<bool> {
+    pub(crate) fn flush(&self) -> Result<()> {
+        self.0.open_tree(INPUTS_HASH_KEY)?.flush()?;
+        self.0.open_tree(TX_INDEX_KEY)?.flush()?;
+        Ok(())
+    }
+
+    pub(crate) fn txid_exists(&self, txid: &Txid) -> Result<bool> {
         let key_bytes = txid.as_raw_hash().to_byte_array().to_vec();
         let tx_inner_bytes = self.0.open_tree(TX_INDEX_KEY)?;
         let tx_inner_bytes = tx_inner_bytes.get(key_bytes)?;
@@ -49,22 +65,78 @@ impl Database {
         let tx_inner_bytes = bincode::serialize(&tx_inner)?;
         tree.insert(&key_bytes, tx_inner_bytes)?;
         self.flush()?;
+
         Ok(())
     }
 
     pub(crate) fn insert_mempool_tx(&self, tx: Transaction) -> Result<()> {
         let tree = self.0.open_tree(TX_INDEX_KEY)?;
         let key_bytes = tx.compute_txid().as_raw_hash().to_byte_array().to_vec();
-        let tx_inner = TransactionInner::new(tx);
+        let tx_inner = TransactionInner::new(tx.clone());
         let tx_inner_bytes = bincode::serialize(&tx_inner)?;
 
+        tree.insert(&key_bytes, tx_inner_bytes)?;
+        let inputs_hash = self.get_inputs_hash(tx.input)?;
+        let inputs_hash_tree = self.0.open_tree(INPUTS_HASH_KEY)?;
+        inputs_hash_tree.insert(&inputs_hash, key_bytes)?;
+
+        self.flush()?;
+        Ok(())
+    }
+
+    /// Returns the txid of the transaction that has the same inputs as the given transaction.
+    pub(crate) fn inputs_hash(
+        &self,
+        inputs: impl IntoIterator<Item = TxIn>,
+    ) -> Result<Option<Txid>> {
+        let inputs_hash = self.get_inputs_hash(inputs)?;
+        let inputs_hash_tree = self.0.open_tree(INPUTS_HASH_KEY)?;
+        let key_bytes = inputs_hash_tree.get(inputs_hash)?;
+        if let Some(key_bytes) = key_bytes {
+            let byte_array = key_bytes
+                .to_vec()
+                .try_into()
+                .map_err(|_| anyhow::anyhow!("Invalid key bytes"))?;
+            let txid = Txid::from_byte_array(byte_array);
+            Ok(Some(txid))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub(crate) fn record_rbf(&self, transaction: Transaction, fee_total: u64) -> Result<()> {
+        let tree = self.0.open_tree(TX_INDEX_KEY)?;
+        let key_bytes = transaction
+            .compute_txid()
+            .as_raw_hash()
+            .to_byte_array()
+            .to_vec();
+        let tx_inner_bytes = tree
+            .get(key_bytes.clone())?
+            .ok_or(anyhow::anyhow!("Transaction not found"))?;
+        let mut tx_inner: TransactionInner = bincode::deserialize(&tx_inner_bytes)?;
+        tx_inner.rbf_inner.push(RBFInner {
+            created_at: SystemTime::now(),
+            fee_total,
+        });
+        let tx_inner_bytes = bincode::serialize(&tx_inner)?;
         tree.insert(&key_bytes, tx_inner_bytes)?;
         self.flush()?;
         Ok(())
     }
 
-    pub(crate) fn flush(&self) -> Result<()> {
-        self.0.flush()?;
-        Ok(())
+    fn get_inputs_hash(&self, inputs: impl IntoIterator<Item = TxIn>) -> Result<Vec<u8>> {
+        let mut engine = Sha256::engine();
+        for i in inputs {
+            let mut writer = vec![];
+            i.consensus_encode(&mut writer)
+                .expect("encoding doesn't error");
+            std::io::copy(&mut writer.as_slice(), &mut engine).expect("engine writes don't error");
+        }
+
+        let hash = Sha256::from_engine(engine);
+        let hash_bytes = hash.as_byte_array().to_vec();
+        println!("Inputs hash: {:?}", hash_bytes);
+        Ok(hash_bytes)
     }
 }
